3f9adb795c3d5f37d88a37c9951eb4483b071876
Alejandro A. Edera <edera.alejandro@gmail.com>
2021-03-02 09:45:33 -0300

    Remove logger

--- anc2vec/main.py
+++ anc2vec/main.py
@@ -6,16 +6,11 @@ import numpy as np
 
 from . import utils
 
-logger = logging.getLogger(__name__)
-
 def get_embeddings():
     tmpdir = tempfile.gettempdir()
     emb_fin = os.path.join(tmpdir, 'embeddings.npz')
 
     if not os.path.exists(emb_fin):
-        logger.info(
-            "Start downloading pre-trained Anc2Vec embeddings (~35MB)..."
-        )
         utils.download_file_from_google_drive(
             '13DMaWYi-zBF8hbDFvIJM5p5vGb6kUPf3', emb_fin)
 
