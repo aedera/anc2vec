54a91d06f3b0ef2c39e33abb2d06aa2d05fb373f
Alejandro A. Edera <edera.alejandro@gmail.com>
2021-03-02 11:12:07 -0300

    Adjusting fit method

--- anc2vec/train/train.py
+++ anc2vec/train/train.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import sys
 import datetime
+import tempfile
 
 import tensorflow as tf
 
@@ -10,15 +11,14 @@ from .models import Embedder
 from .dataset import Dataset
 
 def define_callbacks(model_name):
-    ##
-    models_dir = 'models/' + model_name + '/'
+    tmpdir = tempfile.gettempdir()
+    models_dir = tmpdir + '/models/' + model_name + '/'
     datetime_tag = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
-    log_dir = "logs/" + model_name + '/' + datetime_tag
+    log_dir = tmpdir + "/logs/" + model_name + '/' + datetime_tag
     tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1)
 
-    #breakpoint()
     callbacks = [
-        # tf.keras.callbacks.ModelCheckpoint(filepath=models_dir + datetime_tag + '!{epoch:05d}.tf'),
+        tf.keras.callbacks.ModelCheckpoint(filepath=models_dir + datetime_tag + '!{epoch:05d}.tf'),
         # tensorboard_callback
     ]
 
@@ -34,7 +34,7 @@ def fit(obo_fin, embedding_sz=2, batch_sz=4, num_epochs=10):
                         buffer_sz,
                         shuffle=True,
                         seed=1234).build()
-    #train_set = train_set.take(tok.vocab_sz).cache()
+    train_set = train_set.take(tok.vocab_sz).cache()
 
     model = Embedder.build(tok.vocab_sz, embedding_sz)
     print(model.summary())
@@ -44,5 +44,7 @@ def fit(obo_fin, embedding_sz=2, batch_sz=4, num_epochs=10):
               callbacks=define_callbacks(model.name + '_embedding_sz=' + str(embedding_sz)))
 
     # load model with best loss
-    #tf.models.load_model(
-    #return {'term2index': tok.term2index, 'embeddings': model.get_layer('embedding').numpy()}
+    tmpdir = tempfile.gettempdir()
+    breakpoint()
+    model = tf.models.load_model(tmpdir + '/models/' + model.name)
+    return {'term2index': tok.term2index, 'embeddings': model.get_layer('embedding').numpy()}
